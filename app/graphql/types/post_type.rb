a0adf171ca12e5100669332a508898180f59fd0c
chsohn15 <chsohn15@gmail.com>
2020-11-19 23:38:18 -0500

    create query types

--- app/graphql/types/post_type.rb
+++ app/graphql/types/post_type.rb
@@ -1,11 +1,9 @@
 module Types
   class PostType < Types::BaseObject
     field :id, ID, null: false
-    field :user_id, Integer, null: false
-    field :title, String, null: true
-    field :body, String, null: true
-    field :text, String, null: true
-    field :created_at, GraphQL::Types::ISO8601DateTime, null: false
-    field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+    #field :user_id, Integer, null: false
+    field :title, String, null: false
+    field :body, String, null: false
+    #field :text, String, null: true
   end
 end
